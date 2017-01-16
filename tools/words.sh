055054dfa414e8cf931687dcef4616210ec6e3aa
Daniel Reaser <daniel@reaser.me>
2017-01-16 04:16:19 -0600

    added threads

--- tools/words.sh
+++ tools/words.sh
@@ -21,7 +21,7 @@ if [ ! -e $VECTOR_DATA ]; then
   fi
   echo -----------------------------------------------------------------------------------------------------
   echo -- Training vectors...
-  time $BIN_DIR/word2vec -train $TEXT_DATA -output $VECTOR_DATA -cbow 0 -size 200 -window 5 -negative 0 -hs 1 -sample 1e-3 -threads 12 -binary 1
+  time $BIN_DIR/word2vec -train $TEXT_DATA -output $VECTOR_DATA -cbow 0 -size 200 -window 5 -negative 0 -hs 1 -sample 1e-3 -threads 32 -binary 1
 
 fi
 
